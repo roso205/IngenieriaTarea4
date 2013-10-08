3425cabb06addf7df56d464dbe46abac915baacf
Arturo <arturo.voltattorni@gmail.com>
2013-10-07 23:57:34 -0430

    Main hecho por Karla

--- IngenieriaTarea4/src/Controladores/Main.java
+++ IngenieriaTarea4/src/Controladores/Main.java
@@ -4,45 +4,43 @@
  */
 package Controladores;
 
-import Controladores.*;
-import javax.swing.JOptionPane;
-import java.util.*;
-import java.text.*;
-import ObjetosBase.*;
-import Interface.*;
-
+import java.util.Scanner;
+import ObjetosBase.Cliente;
 
 /**
- * 
  *
- * @author arturo
+ * @author karla
  */
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main (String [] args) {
         
+        Scanner en = new Scanner(System.in);
+        System.out.println("Ingrese su documento de identificacion");
         
-       //PlanPostpago Puta = new PlanPostpago(5,"hola","chao",9);
+        int rif = en.nextInt();
+        ControladorConsumidores Consulta = new ControladorConsumidores();
+        Cliente cli = Consulta.buscarCliente(rif);
         
-     //  Puta.ImprimirFactura(74223);
-
-
+        System.out.println("Bienvenido Sr(a). " + cli.getNombre() 
+                + "\n Seleccione la opcion de su preferencia: \n" 
+                + "1- Generar factura \n" + "2- Agregar servicio adicional");
         
-      ControladorFacturas Factura = new ControladorFacturas();
+        int sel = en.nextInt();
         
-      Factura.ImprimirFactura(74223);
-      
-      
+        switch(sel) {
         
-        //Afiliaciones hola = new Afiliaciones();
-        
-       // hola.modificarPlan(77854, 27564);
-        
-        
-       // Date fecha = new Date();
-      //  ControladorConsumos Consumo = new ControladorConsumos();
-       // Consumo.agregarConsumo(1000000, 1014, fecha , 74223);
-      
+            case 1: System.out.println("Ingrese el codigo del producto");
+                    int codProd = en.nextInt();
+                    ControladorFacturas Consulta2 = new ControladorFacturas();
+                    Consulta2.ImprimirFactura(codProd);
+                
+            case 2: ControlarorServiciosContratables cnt = new ControlarorServiciosContratables();
+                    cnt.ContratarServicio();
+                
+            default:    
+                System.out.println("Ingreso una opcion invalida");
+        }
+    
     }
-        
 }
